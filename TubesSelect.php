0f3a64d114c60c736e30b4adf74707b5b682a5cd
Bima-Rahmadani-Arifandi <arifandi.bima@gmail.com>
2025-12-02 20:17:49 +0700

    conn and basic html

---
+++ TubesSelect.php
@@ -0,0 +1,59 @@
+<?php require "conn.php";
+session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href = "tubesSelectStyles.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="header">
+    <div class="back-btn">⮌</div>
+
+    <div class="profile-card">
+        <div class="profile-icon"></div>
+        <div class="profile-text">
+            <b>Nama:</b> <?php ?><br>
+            <b>NIK:</b> 2009181391<br>
+            <b>Nama Matkul: </b> MatkulA
+        </div>
+    </div>
+
+</div>
+
+<div class="container">
+    <div class="card">
+        AIF12313123<br>
+        NAMA MAT
+    </div>
+    <div class="card">
+        AIF123123<br>
+        Matkul00
+    </div>
+    <div class="card">
+        AIF123124<br>
+        Matkul91
+    </div>
+    <div class="card">
+        AIF12313123<br>
+        NAMA MAT
+    </div>
+    <div class="card">
+        AIF12313123<br>
+        NAMA MAT
+    </div>
+    <div class="card">
+        AIF12313123<br>
+        NAMA MAT
+    </div>
+    <div class="card">
+        AIF12313123<br>
+        NAMA MAT
+    </div>
+</div>
+
+</body>
+</html>
