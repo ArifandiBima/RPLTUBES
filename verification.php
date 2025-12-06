2ba02f02fc154b76bdfc5ff087018448962a756b
unknown <srmatthewsetiawan@gmail.com>
2025-12-06 19:37:04 +0700

    add login and verification

---
+++ verification.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+require_once 'config.php';
+
+$input_username = $_POST['username'];
+$input_password = $_POST['password'];
+
+$sql = "SELECT username, tipePengguna FROM pengguna 
+        WHERE username = ? AND pass = ?";
+        
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $input_username, $input_password);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 1) {
+    $user = $result->fetch_assoc();
+    
+    $_SESSION['username'] = $row['username'];
+    $_SESSION['user_type'] = $row['tipePengguna'];
+    $_SESSION['login_time'] = date('Y-m-d H:i:s'); 
+
+} else {
+    header("Location: login.html?error=true");
+}
+
+$stmt->close();
+$conn->close();
+?>
