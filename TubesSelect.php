0c39fe9f0629583039c8c4c9a1f11eed87e587a0
Bima-Rahmadani-Arifandi <arifandi.bima@gmail.com>
2025-12-08 17:39:57 +0700

    pemilihan tubes

--- TubesSelect.php
+++ TubesSelect.php
@@ -16,8 +16,8 @@ session_start();?>
     <div class="profile-card">
         <div class="profile-icon"></div>
         <div class="profile-text">
-            <b>Nama:</b> <?php ?><br>
-            <b>NIK:</b> 2009181391<br>
+            <b>Nama:</b> <?php echo $_SESSION["nama"]??"not set";?><br>
+            <b>NIK:</b> <?php echo $_SESSION["nik"]?? "not set";?><br>
             <b>Nama Matkul: </b> MatkulA
         </div>
     </div>
@@ -25,35 +25,30 @@ session_start();?>
 </div>
 
 <div class="container">
-    <div class="card">
-        AIF12313123<br>
-        NAMA MAT
+    <div class = "card">
+        Pembuatan Keju
     </div>
-    <div class="card">
-        AIF123123<br>
-        Matkul00
-    </div>
-    <div class="card">
-        AIF123124<br>
-        Matkul91
-    </div>
-    <div class="card">
-        AIF12313123<br>
-        NAMA MAT
-    </div>
-    <div class="card">
-        AIF12313123<br>
-        NAMA MAT
-    </div>
-    <div class="card">
-        AIF12313123<br>
-        NAMA MAT
-    </div>
-    <div class="card">
-        AIF12313123<br>
-        NAMA MAT
-    </div>
+    <?php
+    $queryTubes = "
+    SELECT namaTugasBesar
+    FROM tugasBesar
+    WHERE kodeMataKuliah = ? AND kodeKelas =? AND semester = ? 
+    ";
+    $stmt = $conn->prepare($queryTubes);
+    $stmt->bind_param("ssi",$_GET["kodeMataKuliah"], $_GET["kodeKelas"],$_GET["semester"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="card">'.$row["namaTugasBesar"].'</div>';
+    }
+    ?>
 </div>
+<?php
+if ($_SESSION["tipePengguna"]="dosen"){
+    echo '<button type="button"> Make a New Tubes</button>';
+}
+?>
 
 </body>
 </html>
