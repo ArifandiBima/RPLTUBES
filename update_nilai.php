72027f9d050bf2d0fccb8f83d1bf4383d5c11d4f
Christopher Stevenson <christjason28@gmail.com>
2025-12-09 20:51:27 +0700

    Update update_nilai.php

--- update_nilai.php
+++ update_nilai.php
@@ -1,25 +1,16 @@
 <?php
 require "conn.php";
 
-<<<<<<< HEAD
 $namaTugasBesar = $_POST["namaTugasBesar"];
 $kodeMatkul     = $_POST["kodeMatkul"];
 $kelas          = $_POST["kelas"];
 $semester       = (int) $_POST["semester"];
-=======
-// These should come from hidden inputs in the form
-$namaTugasBesar = $_POST["namaTugasBesar"];
-$kodeMatkul     = $_POST["kodeMatkul"];
-$kelas          = $_POST["kelas"];
-$semester       = $_POST["semester"];
->>>>>>> 29117f52e4cf377569b6e6d0fe975986e375bb66
 
 $nilai1 = $_POST['nilai_1'];
 $nilai2 = $_POST['nilai_2'];
 
 foreach ($nilai1 as $npm => $n1) {
 
-<<<<<<< HEAD
     // ---------- UPDATE / INSERT KOMPONEN 1 ----------
     $sql = "SELECT 1 FROM nilai
             WHERE namaTugasBesar=? AND kodeMataKuliah=? 
@@ -79,65 +70,6 @@ foreach ($nilai1 as $npm => $n1) {
     }
 }
 
-
-=======
-    // ========================
-    // UPDATE KOMPONEN 1
-    // ========================
-    $sql1 = "UPDATE nilai
-            SET nilai = ?
-            WHERE namaTugasBesar = ?
-              AND kodeMataKuliah = ?
-              AND kodeKelas = ?
-              AND semester = ?
-              AND nomorKomponen = 1
-              AND npmPeserta = ?";
-
-    $stmt1 = $conn->prepare($sql1);
-
-    // d s s s i s
-    $stmt1->bind_param(
-        "dssssis",
-        $n1,
-        $namaTugasBesar,
-        $kodeMatkul,
-        $kelas,
-        $semester,
-        $npm
-    );
-
-    $stmt1->execute();
-
-    // ========================
-    // UPDATE KOMPONEN 2
-    // ========================
-    $n2 = $nilai2[$npm];
-
-    $sql2 = "UPDATE nilai
-            SET nilai = ?
-            WHERE namaTugasBesar = ?
-              AND kodeMataKuliah = ?
-              AND kodeKelas = ?
-              AND semester = ?
-              AND nomorKomponen = 2
-              AND npmPeserta = ?";
-
-    $stmt2 = $conn->prepare($sql2);
-
-    $stmt2->bind_param(
-        "dssssis",
-        $n2,
-        $namaTugasBesar,
-        $kodeMatkul,
-        $kelas,
-        $semester,
-        $npm
-    );
-
-    $stmt2->execute();
-}
-
->>>>>>> 29117f52e4cf377569b6e6d0fe975986e375bb66
 header("Location: nilai_dosen.php?saved=1");
 exit;
 ?>
